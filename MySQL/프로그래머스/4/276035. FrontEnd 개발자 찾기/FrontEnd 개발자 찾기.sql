0a6fb313f229f97a426224bcddb08d2748cd14cd
Jiin Park <62392317+nagi0101@users.noreply.github.com>
2024-03-01 13:19:30 +0900

    [level 4] Title: FrontEnd 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

--- MySQL/프로그래머스/4/276035. FrontEnd 개발자 찾기/FrontEnd 개발자 찾기.sql
+++ MySQL/프로그래머스/4/276035. FrontEnd 개발자 찾기/FrontEnd 개발자 찾기.sql
@@ -6,5 +6,5 @@ with c as (
     
 select ID, EMAIL, FIRST_NAME, LAST_NAME
 from DEVELOPERS
-where (select s from c where CATEGORY = "Front End") & SKILL_CODE > 0
+where SKILL_CODE & (select s from c where CATEGORY = "Front End") != 0
 order by ID
